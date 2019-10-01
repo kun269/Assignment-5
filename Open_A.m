059a174b61f3d44ebfbd52b75a86e7e0ce237c0b
kun269 <55985732+kun269@users.noreply.github.com>
2019-10-01 19:39:13 -0400

    Add files via upload

---
+++ Open_A.m
@@ -0,0 +1,9 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Assignment 5
+% Open A containing given .txt file
+% Keeley Naylor
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+load Assignment5Data.txt
+
+A = Assignment5Data;
