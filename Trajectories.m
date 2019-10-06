894dcd7b6fa2340852083db9ab1f416ffd445611
kun269 <55985732+kun269@users.noreply.github.com>
2019-10-06 16:28:50 -0400

    Update Trajectories.m

--- Trajectories.m
+++ Trajectories.m
@@ -6,31 +6,13 @@
 % Programmer 1
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+t = A(:,1); % time data
+x = A(:,[2,5,8,11,14,17,20,23,26,29]); % x positions
+y = A(:,[3,6,9,12,15,18,21,24,27,30]); % y positions
+z = A(:,[4,7,10,13,16,19,22,25,28,31]); % z positions
 
-t = A(:,1) % time data
-x = A(:,[2,5,8,11,14,17,20,23,26,29]) % x positions
-y = A(:,[3,6,9,12,15,18,21,24,27,30]) % y positions
-z = A(:,[4,7,10,13,16,19,22,25,28,31]) % z positions
 
-figure(1) % plot x over time
-plot(t,x)
-title('x position vs. time')
-xlabel('Time (s)')
-ylabel('Position')
-
-figure(2) % plot y over time
-plot(t,y)
-title('y position vs. time')
-xlabel('Time (s)')
-ylabel('Position')
-
-figure(3)  % plot z over time
-plot(t,z)
-title('z position vs. time')
-xlabel('Time (s)')
-ylabel('Position')
-
-figure(4) % combine all position plots 
+figure(1) % combine all position plots 
 hold on
 plot(t,x)
 plot(t,y)
@@ -40,3 +22,21 @@ xlabel('Time (s)')
 ylabel('Position')
 
 legend('x','y','z') % define each plot based on color
+
+% figure1(2) % plot x over time (not in main function)
+% plot(t,x)
+% title('x position vs. time')
+% xlabel('Time (s)')
+% ylabel('Position')
+
+% figure(3) % plot y over time (not in main function)
+% plot(t,y)
+% title('y position vs. time')
+% xlabel('Time (s)')
+% ylabel('Position')
+
+% figure(4)  % plot z over time (not in main function)
+% plot(t,z)
+% title('z position vs. time')
+% xlabel('Time (s)')
+% ylabel('Position')
