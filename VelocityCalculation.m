58d61b1fb36979bf6dbf2070d681ccae59833827
maa427 <56008880+maa427@users.noreply.github.com>
2019-10-06 20:02:32 -0400

    Update VelocityCalculation.m
    Changed the "figure" functions place and move it  outside the for loop the fix the error. Used tabs inside the for loops to make my code more readable.

--- VelocityCalculation.m
+++ VelocityCalculation.m
@@ -19,35 +19,31 @@ function [] = VelocityCalculation(A)
 load A.mat
 
 for j=1:10
-for i=2:141
-    
-V(i,j)=sqrt((A(i,3*j-1)-A(i-1,3*j-1))^2+(A(i,3*j)-A(i-1,3*j))^2+(A(i,3*j+1)-A(i-1,3*j+1))^2)/(A(i,1)-A(i-1,1));
-% Velocity is approximated as displacement between two point divided by the
-% time interval
-%Displacement: (sqrt((Xi-Xi-1)^2+(Yi-Yi-1)^2+(Zi-Zi-1)^2)
-%Time:(ti-ti-1)
+    for i=2:141
 
-
-end
+        V(i,j)=sqrt((A(i,3*j-1)-A(i-1,3*j-1))^2+(A(i,3*j)-A(i-1,3*j))^2+(A(i,3*j+1)-A(i-1,3*j+1))^2)/(A(i,1)-A(i-1,1));
+        % Velocity is approximated as displacement between two point divided by the
+        % time interval
+        %Displacement: (sqrt((Xi-Xi-1)^2+(Yi-Yi-1)^2+(Zi-Zi-1)^2)
+        %Time:(ti-ti-1)
+    end
 end
 
-
-
+figure
 for i=1:10
-subplot(2,5,i)
-c = polyfit(A(:,1),V(:,i),2); %Polynomial curve fitting is applied to estimate the velocity
+    subplot(2,5,i)
+    c = polyfit(A(:,1),V(:,i),2); %Polynomial curve fitting is applied to estimate the velocity
 
-Fline = polyval(c,A(:,1));
-    figure
+    Fline = polyval(c,A(:,1));
+
     plot(A(:,1),V(:,i)) %Approximated velocity is plotted
     hold on
     plot(A(:,1),Fline,'r--','LineWidth',2) %Estimation for velocity is plotted
     hold off
-title(['Experiment ',num2str(i)])
-xlabel('Time(s)')
-ylabel('Velocity(m/s)')
-legend('Approximate velocity','Estimated velocity')
-end
-    
+    title(['Experiment ',num2str(i)])
+    xlabel('Time(s)')
+    ylabel('Velocity(m/s)')
+    legend('Approximate velocity','Estimated velocity')
+    end
     
 end
